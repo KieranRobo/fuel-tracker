e183cfc25132f9f503a53affeb3f5cda5d5dd493
Kieran Robertson <kieran.robertson.2013@uni.strath.ac.uk>
2019-08-04 23:06:02 +0100

    Basic form inputs

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -4,22 +4,26 @@ import { Route, Switch, Link } from 'react-router-dom';
 import OwnedVehicles from './OwnedVehicles';
 import FuelView from './FuelView';
 import FillUp from './FillIUp';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/Button';
 
 
 
 function App() {
   return (
     <div className="App">
-        
-        <div><Link to="/">Home</Link></div>
-        <div><Link to="/vehicles">Owned Vehicles</Link></div>
+
+        <ButtonGroup>
+          <Link to="/"><Button variant="secondary">Home</Button></Link>
+          <Link to="/vehicles"><Button variant="secondary">Owned Vehicles</Button></Link>
+          <Button variant="secondary">Logout</Button>
+        </ButtonGroup>
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/vehicles" component={OwnedVehicles} />
           <Route exact path="/fuel/:vehicle" component={FuelView} />
           <Route exact path="/fuel/:vehicle/fill-up" component={FillUp} />
         </Switch>
-        
         
     </div>
   );
--- frontend/src/components/FillIUp.js
+++ frontend/src/components/FillIUp.js
@@ -1,14 +1,37 @@
 import React from 'react';
 
 import axios from "../api.service";
-
-
+import {Form} from 'react-bootstrap';
+import {Button} from 'react-bootstrap'
 
 class FillUp extends React.Component {
 
-    state = {
-        ownedVehicle: { vehicle : null }
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            ownedVehicle: { vehicle : null },
+            currentMileage: '',
+            litres: '',
+            cost: ''
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+        this.mileageChange = this.mileageChange.bind(this);
+        this.litresChange = this.litresChange.bind(this);
+        this.costChange = this.costChange.bind(this);
+        
+    }
+
+    mileageChange(event) { this.setState({ currentMileage: event.target.value }) }
+    litresChange(event) { this.setState({ litres: event.target.value }) }
+    costChange(event) { this.setState({ cost: event.target.value }) }
+
+    handleSubmit(event) {
+        console.log(this.state.cost);
+        event.preventDefault();
+    }
 
     componentDidMount() {
         let vehicleId = this.props.match.params.vehicle;
@@ -32,7 +55,24 @@ class FillUp extends React.Component {
             )
         }
         return (
-            <div>You are filling up your {this.state.ownedVehicle.vehicle.brand} {this.state.ownedVehicle.vehicle.model}</div>
+            <div>
+                <h2>Fill Up</h2>
+                <div>Lets track a new fill up for your {this.state.ownedVehicle.vehicle.brand} {this.state.ownedVehicle.vehicle.model}</div>
+                <hr></hr>
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Label>What is your <strong>current mileage</strong>?</Form.Label>
+                    <Form.Control size="lg" type="text" value={this.state.currentMileage} onChange={this.mileageChange} /><br />
+
+                    <Form.Label>How much <strong>litres</strong> did you fill up?</Form.Label>
+                    <Form.Control size="lg" type="text" value={this.state.litres} onChange={this.litresChange} /><br />
+
+                    <Form.Label>How much did this fill up <strong>cost</strong>?</Form.Label>
+                    <Form.Control size="lg" type="text" value={this.state.cost} onChange={this.costChange} /><br />
+
+                    <Button type="submit" size="lg">Track It!</Button>
+                </Form>
+            </div>
+
         );
     }
 }
